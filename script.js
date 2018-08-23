14034aae8a5f549c7aeb42e6bef05b9cbe5267ac
Danilo Porto <danilo.porto@qitech.work>
2018-08-23 16:42:26 -0300

    Pages

---
+++ script.js
@@ -0,0 +1,257 @@
+var JSONdashboard = {
+	"overview": {
+		"drawMode": "color",
+		"theme": "light",
+		"graphs": [
+			{
+				"title": "Meu titulo",
+				"google_id": "12535654214181780356",
+				"timeframe": "1h",
+				"autorefresh": "true",
+				"showLegend": "true"
+			},
+			{
+				"title": "Meu titulo",
+				"google_id": "2095215764691479119",
+				"timeframe": "1h",
+				"autorefresh": "true",
+				"showLegend": "true"
+			},
+			{
+				"title": "Meu titulo",
+				"google_id": "4639022294800159216",
+				"timeframe": "1h",
+				"autorefresh": "true",
+				"showLegend": "true"
+			},
+			{
+				"title": "Meu titulo",
+				"google_id": "8747911624461323110",
+				"timeframe": "1h",
+				"autorefresh": "true",
+				"showLegend": "true"
+			}
+		]
+	},
+	"overviewExtra": {
+		"drawMode": "color",
+		"theme": "light",
+		"graphs": [
+			{
+				"title": "Meu titulo",
+				"google_id": "13273750352647881124",
+				"timeframe": "1h",
+				"autorefresh": "true",
+				"showLegend": "true"
+			},
+			{
+				"title": "Meu titulo",
+				"google_id": "4639022294800160975",
+				"timeframe": "1h",
+				"autorefresh": "true",
+				"showLegend": "true"
+			},
+			{
+				"title": "Meu titulo",
+				"google_id": "16977468836365572409",
+				"timeframe": "1h",
+				"autorefresh": "true",
+				"showLegend": "true"
+			},
+			{
+				"title": "Meu titulo",
+				"google_id": "8747911624461323110",
+				"timeframe": "1h",
+				"autorefresh": "true",
+				"showLegend": "true"
+			}
+		]
+	},
+	"SQLdb": {
+		"drawMode": "color",
+		"theme": "light",
+		"graphs": [
+			{
+				"title": "Meu titulo",
+				"google_id": "15524514366712537707",
+				"timeframe": "1h",
+				"autorefresh": "true",
+				"showLegend": "true"
+			},
+			{
+				"title": "Meu titulo",
+				"google_id": "10981039593291568802",
+				"timeframe": "1h",
+				"autorefresh": "true",
+				"showLegend": "true"
+			},
+			{
+				"title": "Meu titulo",
+				"google_id": "4042710973940922638",
+				"timeframe": "1h",
+				"autorefresh": "true",
+				"showLegend": "true"
+			},
+			{
+				"title": "Meu titulo",
+				"google_id": "8747911624461323110",
+				"timeframe": "1h",
+				"autorefresh": "true",
+				"showLegend": "true"
+			}
+		]
+	},
+
+	"account": {
+		"graphURL1":"https://public.google.stackdriver.com/public/chart/590314631763103316?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL2":"https://public.google.stackdriver.com/public/chart/2371923162729352133?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL3":"https://public.google.stackdriver.com/public/chart/12473177168262476028?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL4":"https://public.google.stackdriver.com/public/chart/923128817732870430?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h"
+	},
+	"compliance": {
+		"graphURL1":"https://public.google.stackdriver.com/public/chart/590314631763103316?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL2":"https://public.google.stackdriver.com/public/chart/2371923162729352133?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL3":"https://public.google.stackdriver.com/public/chart/12473177168262476028?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL4":"https://public.google.stackdriver.com/public/chart/923128817732870430?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h"
+	},
+	"creditoperation": {
+		"graphURL1":"https://public.google.stackdriver.com/public/chart/590314631763103316?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL2":"https://public.google.stackdriver.com/public/chart/2371923162729352133?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL3":"https://public.google.stackdriver.com/public/chart/12473177168262476028?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL4":"https://public.google.stackdriver.com/public/chart/923128817732870430?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h"
+	},
+	"di": {
+		"graphURL1":"https://public.google.stackdriver.com/public/chart/590314631763103316?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL2":"https://public.google.stackdriver.com/public/chart/2371923162729352133?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL3":"https://public.google.stackdriver.com/public/chart/12473177168262476028?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL4":"https://public.google.stackdriver.com/public/chart/923128817732870430?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h"
+	},
+	"dash": {
+		"graphURL1":"https://public.google.stackdriver.com/public/chart/590314631763103316?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL2":"https://public.google.stackdriver.com/public/chart/2371923162729352133?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL3":"https://public.google.stackdriver.com/public/chart/12473177168262476028?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL4":"https://public.google.stackdriver.com/public/chart/923128817732870430?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h"
+	},
+	"doc": {
+		"graphURL1":"https://public.google.stackdriver.com/public/chart/590314631763103316?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL2":"https://public.google.stackdriver.com/public/chart/2371923162729352133?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL3":"https://public.google.stackdriver.com/public/chart/12473177168262476028?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL4":"https://public.google.stackdriver.com/public/chart/923128817732870430?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h"
+	},
+	"keycloak": {
+		"graphURL1":"https://public.google.stackdriver.com/public/chart/590314631763103316?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL2":"https://public.google.stackdriver.com/public/chart/2371923162729352133?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL3":"https://public.google.stackdriver.com/public/chart/12473177168262476028?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL4":"https://public.google.stackdriver.com/public/chart/923128817732870430?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h"
+	},
+	"leadmanagement": {
+		"graphURL1":"https://public.google.stackdriver.com/public/chart/590314631763103316?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL2":"https://public.google.stackdriver.com/public/chart/2371923162729352133?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL3":"https://public.google.stackdriver.com/public/chart/12473177168262476028?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL4":"https://public.google.stackdriver.com/public/chart/923128817732870430?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h"
+	},
+	"onboarding": {
+		"graphURL1":"https://public.google.stackdriver.com/public/chart/590314631763103316?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL2":"https://public.google.stackdriver.com/public/chart/2371923162729352133?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL3":"https://public.google.stackdriver.com/public/chart/12473177168262476028?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL4":"https://public.google.stackdriver.com/public/chart/923128817732870430?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h"
+	},
+	"psti": {
+		"graphURL1":"https://public.google.stackdriver.com/public/chart/590314631763103316?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL2":"https://public.google.stackdriver.com/public/chart/2371923162729352133?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL3":"https://public.google.stackdriver.com/public/chart/12473177168262476028?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL4":"https://public.google.stackdriver.com/public/chart/923128817732870430?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h"
+	},
+	"permission": {
+		"graphURL1":"https://public.google.stackdriver.com/public/chart/590314631763103316?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL2":"https://public.google.stackdriver.com/public/chart/2371923162729352133?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL3":"https://public.google.stackdriver.com/public/chart/12473177168262476028?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL4":"https://public.google.stackdriver.com/public/chart/923128817732870430?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h"
+	},
+	"scheduler": {
+		"graphURL1":"https://public.google.stackdriver.com/public/chart/590314631763103316?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL2":"https://public.google.stackdriver.com/public/chart/2371923162729352133?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL3":"https://public.google.stackdriver.com/public/chart/12473177168262476028?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL4":"https://public.google.stackdriver.com/public/chart/923128817732870430?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h"
+	},
+	"ted": {
+		"graphURL1":"https://public.google.stackdriver.com/public/chart/590314631763103316?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL2":"https://public.google.stackdriver.com/public/chart/2371923162729352133?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL3":"https://public.google.stackdriver.com/public/chart/12473177168262476028?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL4":"https://public.google.stackdriver.com/public/chart/923128817732870430?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h"
+	},
+	"wallet": {
+		"graphURL1":"https://public.google.stackdriver.com/public/chart/590314631763103316?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL2":"https://public.google.stackdriver.com/public/chart/2371923162729352133?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL3":"https://public.google.stackdriver.com/public/chart/12473177168262476028?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL4":"https://public.google.stackdriver.com/public/chart/923128817732870430?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h"
+	},
+	"webhook": {
+		"graphURL1":"https://public.google.stackdriver.com/public/chart/590314631763103316?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL2":"https://public.google.stackdriver.com/public/chart/2371923162729352133?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL3":"https://public.google.stackdriver.com/public/chart/12473177168262476028?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h",
+		"graphURL4":"https://public.google.stackdriver.com/public/chart/923128817732870430?drawMode=color&showLegend=true&theme=light&autoRefresh=true&timeframe=1h"
+	}
+}
+var currentDash = 0;
+setInterval( 
+	function(){
+		var el1 = document.getElementById('iframe1');
+		var el2 = document.getElementById('iframe2');
+		var el3 = document.getElementById('iframe3');
+		var el4 = document.getElementById('iframe4');
+		switch(currentDash){
+			case 0: {
+				el1.src = "\"https://public.google.stackdriver.com/public/chart/" + JSONdashboard.overview.graphs[0].google_id + "?drawMode=" + JSONdashboard.overview.graphs[0].drawMode + 
+				"&showLegend=" + JSONdashboard.overview.graphs[0].showLegend + "&theme=" + JSONdashboard.overview.theme + "&autoRefresh=" + JSONdashboard.overview.graphs[0].autorefresh + 
+				"&timeframe=" + JSONdashboard.overview.graphs[0].timeframe;
+				el2.src = "\"https://public.google.stackdriver.com/public/chart/" + JSONdashboard.overview.graphs[1].google_id + "?drawMode=" + JSONdashboard.overview.graphs[1].drawMode + 
+				"&showLegend=" + JSONdashboard.overview.graphs[1].showLegend + "&theme=" + JSONdashboard.overview.theme + "&autoRefresh=" + JSONdashboard.overview.graphs[1].autorefresh + 
+				"&timeframe=" + JSONdashboard.overview.graphs[1].timeframe;
+				el3.src = "\"https://public.google.stackdriver.com/public/chart/" + JSONdashboard.overview.graphs[2].google_id + "?drawMode=" + JSONdashboard.overview.graphs[2].drawMode + 
+				"&showLegend=" + JSONdashboard.overview.graphs[2].showLegend + "&theme=" + JSONdashboard.overview.theme + "&autoRefresh=" + JSONdashboard.overview.graphs[2].autorefresh + 
+				"&timeframe=" + JSONdashboard.overview.graphs[2].timeframe;
+				el4.src = "\"https://public.google.stackdriver.com/public/chart/" + JSONdashboard.overview.graphs[3].google_id + "?drawMode=" + JSONdashboard.overview.graphs[3].drawMode + 
+				"&showLegend=" + JSONdashboard.overview.graphs[3].showLegend + "&theme=" + JSONdashboard.overview.theme + "&autoRefresh=" + JSONdashboard.overview.graphs[3].autorefresh + 
+				"&timeframe=" + JSONdashboard.overview.graphs[3].timeframe;
+				h1.textContent = "Visão geral";
+				stringUsed = 1;
+				break;
+			}
+			case 1: {
+				el1.src = "\"https://public.google.stackdriver.com/public/chart/" + JSONdashboard.overviewExtra.graphs[0].google_id + "?drawMode=" + JSONdashboard.overviewExtra.graphs[0].drawMode + 
+				"&showLegend=" + JSONdashboard.overviewExtra.graphs[0].showLegend + "&theme=" + JSONdashboard.overviewExtra.theme + "&autoRefresh=" + JSONdashboard.overviewExtra.graphs[0].autorefresh + 
+				"&timeframe=" + JSONdashboard.overviewExtra.graphs[0].timeframe;
+				el2.src = "\"https://public.google.stackdriver.com/public/chart/" + JSONdashboard.overviewExtra.graphs[1].google_id + "?drawMode=" + JSONdashboard.overviewExtra.graphs[1].drawMode + 
+				"&showLegend=" + JSONdashboard.overviewExtra.graphs[1].showLegend + "&theme=" + JSONdashboard.overviewExtra.theme + "&autoRefresh=" + JSONdashboard.overviewExtra.graphs[1].autorefresh + 
+				"&timeframe=" + JSONdashboard.overviewExtra.graphs[1].timeframe;
+				el3.src = "\"https://public.google.stackdriver.com/public/chart/" + JSONdashboard.overviewExtra.graphs[2].google_id + "?drawMode=" + JSONdashboard.overviewExtra.graphs[2].drawMode + 
+				"&showLegend=" + JSONdashboard.overviewExtra.graphs[2].showLegend + "&theme=" + JSONdashboard.overviewExtra.theme + "&autoRefresh=" + JSONdashboard.overviewExtra.graphs[2].autorefresh + 
+				"&timeframe=" + JSONdashboard.overviewExtra.graphs[2].timeframe;
+				el4.src = "\"https://public.google.stackdriver.com/public/chart/" + JSONdashboard.overviewExtra.graphs[3].google_id + "?drawMode=" + JSONdashboard.overviewExtra.graphs[3].drawMode + 
+				"&showLegend=" + JSONdashboard.overviewExtra.graphs[3].showLegend + "&theme=" + JSONdashboard.overviewExtra.theme + "&autoRefresh=" + JSONdashboard.overviewExtra.graphs[3].autorefresh + 
+				"&timeframe=" + JSONdashboard.overviewExtra.graphs[3].timeframe;
+				h1.textContent = "Visão geral extra";
+				stringUsed = 2;
+				break;
+			}
+			case 2: {
+				el1.src = "\"https://public.google.stackdriver.com/public/chart/" + JSONdashboard.SQLdb.graphs[0].google_id + "?drawMode=" + JSONdashboard.SQLdb.graphs[0].drawMode + 
+				"&showLegend=" + JSONdashboard.SQLdb.graphs[0].showLegend + "&theme=" + JSONdashboard.SQLdb.theme + "&autoRefresh=" + JSONdashboard.SQLdb.graphs[0].autorefresh + 
+				"&timeframe=" + JSONdashboard.SQLdb.graphs[0].timeframe;
+				el2.src = "\"https://public.google.stackdriver.com/public/chart/" + JSONdashboard.SQLdb.graphs[1].google_id + "?drawMode=" + JSONdashboard.SQLdb.graphs[1].drawMode + 
+				"&showLegend=" + JSONdashboard.SQLdb.graphs[1].showLegend + "&theme=" + JSONdashboard.SQLdb.theme + "&autoRefresh=" + JSONdashboard.SQLdb.graphs[1].autorefresh + 
+				"&timeframe=" + JSONdashboard.SQLdb.graphs[1].timeframe;
+				el3.src = "\"https://public.google.stackdriver.com/public/chart/" + JSONdashboard.SQLdb.graphs[2].google_id + "?drawMode=" + JSONdashboard.SQLdb.graphs[2].drawMode + 
+				"&showLegend=" + JSONdashboard.SQLdb.graphs[2].showLegend + "&theme=" + JSONdashboard.SQLdb.theme + "&autoRefresh=" + JSONdashboard.SQLdb.graphs[2].autorefresh + 
+				"&timeframe=" + JSONdashboard.SQLdb.graphs[2].timeframe;
+				el4.src = "\"https://public.google.stackdriver.com/public/chart/" + JSONdashboard.SQLdb.graphs[3].google_id + "?drawMode=" + JSONdashboard.SQLdb.graphs[3].drawMode + 
+				"&showLegend=" + JSONdashboard.SQLdb.graphs[3].showLegend + "&theme=" + JSONdashboard.SQLdb.theme + "&autoRefresh=" + JSONdashboard.SQLdb.graphs[3].autorefresh + 
+				"&timeframe=" + JSONdashboard.SQLdb.graphs[3].timeframe;
+				h1.textContent = "SQL DB";
+				stringUsed = 0;
+				break;
+			}
+		}
+	}
+, 120000);
